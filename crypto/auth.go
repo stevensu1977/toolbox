0f64981f9fb1cf8937d41ed6598b6d27a8923912
suwei <suwei@orientsoft.cn>
2017-09-13 12:14:24 +0800

    add auth.go auth_test.go

---
+++ crypto/auth.go
@@ -0,0 +1,39 @@
+package crypto
+
+import (
+	"encoding/base64"
+	"strings"
+)
+
+//BasicAuthEncode is HTTP Basic Auth helper func
+func BasicAuthEncode(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+//BaseAuthDecode is HTTP Basic Auth helper func
+func BaseAuthDecode(input string) ([]string, error) {
+
+	data, err := Base64Decode(input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.Split(data, ":"), nil
+
+}
+
+//Base64Encode simple base64 helper func
+func Base64Encode(input string) string {
+	return base64.StdEncoding.EncodeToString([]byte(input))
+}
+
+//Base64Decode simple base64 helpler func
+func Base64Decode(input string) (string, error) {
+	result, err := base64.StdEncoding.DecodeString(input)
+	if err != nil {
+		return "", err
+	}
+	return string(result), nil
+}
---
+++ crypto/auth_test.go
@@ -0,0 +1,27 @@
+package crypto
+
+import (
+	"testing"
+)
+
+func TestBasicAuthEncode(t *testing.T) {
+
+	encodePassword := BasicAuthEncode("wei.su@oracle.com", "a123gjGJ")
+	t.Log(encodePassword)
+	raw, err := Base64Decode(Base64Encode("welcome1"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if raw != "welcome1" {
+		t.Fatal("not match")
+	}
+
+	//str1, err := Base64Decode("dx1212313=")
+	str1, err := Base64Decode(encodePassword)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(str1)
+	t.Log(BaseAuthDecode(encodePassword))
+
+}
