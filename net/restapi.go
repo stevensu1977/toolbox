704e832cc48907f540f82402bd206869f64c0c8b
Steven Su <suwei007@gmail.com>
2017-09-21 22:12:00 +0800

    add func comment for restapi.go

--- net/restapi.go
+++ net/restapi.go
@@ -9,7 +9,8 @@ import (
 )
 
 const (
-	API_JSON = iota //API_JSON
+	//API_JSON json type
+	API_JSON = iota
 	API_XML
 	API_RAW
 )
@@ -19,42 +20,53 @@ type Encoder interface {
 	Encode(v interface{}) error
 }
 
+//XMLEncoder xml encoder
 type XMLEncoder struct {
 	encoder *xml.Encoder
 }
 
+//JSONEncoder json struct
 type JSONEncoder struct {
 	encoder *json.Encoder
 }
 
+//RawEncoder json struct
 type RawEncoder struct {
 	body *bytes.Buffer
 }
 
+//Encode implement Encoder interface
 func (r *RawEncoder) Encode(v interface{}) error {
 	_, err := r.body.Write([]byte(fmt.Sprintf("%v", v)))
 	return err
 }
 
+//Encode implement Encoder interface
 func (j *JSONEncoder) Encode(v interface{}) error {
 	return j.encoder.Encode(v)
 }
+
+//Encode implement Encoder interface
 func (x *XMLEncoder) Encode(v interface{}) error {
 	return x.encoder.Encode(v)
 }
 
-//ServerJSON provide simple REST API JSON write
+//ServerJSON provide simple REST API go struct to JSON  func
 func ServerJSON(w http.ResponseWriter, model interface{}) {
 	serverAPI(API_JSON, w, model)
 }
 
+//ServerXML provide simple REST API go struct to XML func
 func ServerXML(w http.ResponseWriter, model interface{}) {
 	serverAPI(API_XML, w, model)
 }
 
+//ServerRAW provid simple REST API RAW go structure
 func ServerRAW(w http.ResponseWriter, model interface{}) {
 	serverAPI(API_RAW, w, model)
 }
+
+//serverAPI is internal func
 func serverAPI(apiType int, w http.ResponseWriter, model interface{}) {
 	var buf bytes.Buffer
 	var contentWriter Encoder
